0c4279094227903d445eeac34ec8578ef8e2316b
Łukasz Lipiński <llipinski@getresponse.com>
2017-12-11 10:16:10 +0100

    splitting stuff

---
+++ src/config/marks.js
@@ -0,0 +1,42 @@
+// :: Object [Specs](#model.MarkSpec) for the marks in the schema.
+export const marks = {
+    // :: MarkSpec A link. Has `href` and `title` attributes. `title`
+    // defaults to the empty string. Rendered and parsed as an `<a>`
+    // element.
+    link: {
+        attrs: {
+            href: {},
+            title: {default: null}
+        },
+        inclusive: false,
+        parseDOM: [{tag: "a[href]", getAttrs(dom) {
+            return {href: dom.getAttribute("href"), title: dom.getAttribute("title")}
+        }}],
+        toDOM(node) { return ["a", node.attrs] }
+    },
+
+    // :: MarkSpec An emphasis mark. Rendered as an `<em>` element.
+    // Has parse rules that also match `<i>` and `font-style: italic`.
+    em: {
+        parseDOM: [{tag: "i"}, {tag: "em"}, {style: "font-style=italic"}],
+        toDOM() { return ["em"] }
+    },
+
+    // :: MarkSpec A strong mark. Rendered as `<strong>`, parse rules
+    // also match `<b>` and `font-weight: bold`.
+    strong: {
+        parseDOM: [{tag: "strong"},
+            // This works around a Google Docs misbehavior where
+            // pasted content will be inexplicably wrapped in `<b>`
+            // tags with a font-weight normal.
+            {tag: "b", getAttrs: node => node.style.fontWeight != "normal" && null},
+            {style: "font-weight", getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null}],
+        toDOM() { return ["strong"] }
+    },
+
+    // :: MarkSpec Code font mark. Represented as a `<code>` element.
+    code: {
+        parseDOM: [{tag: "code"}],
+        toDOM() { return ["code"] }
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,14 @@ module.exports = {
 			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
 			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
 		]
-	}
+	},
+    resolve: {
+        alias: {
+
+        },
+        modules: [
+            path.resolve(__dirname, 'src'),
+            'node_modules',
+        ]
+    }
 };
